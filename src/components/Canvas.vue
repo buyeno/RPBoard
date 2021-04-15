3887b1fdf3d38835253db2b8b7a027a61cb96d1b
Brian Uyeno <brian_u@bittermelon.io>
2021-04-15 11:50:07 -0600

    vuetify

---
+++ src/components/Canvas.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <canvas id="canvas" width="300" height="300"></canvas>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { fabric } from "fabric";
+
+export default {
+  name: "Canvas",
+  methods: {
+    setCanvas() {
+      var canvas = new fabric.Canvas("canvas");
+
+      var rect = new fabric.Rect({
+        top: 100,
+        left: 100,
+        width: 60,
+        height: 70,
+        fill: "red"
+      });
+      canvas.add(rect);
+    }
+  },
+  mounted() {
+    this.setCanvas();
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Canvas />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Canvas from '@/components/Canvas.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
-  }
+    Canvas,
+  },
 };
 </script>
